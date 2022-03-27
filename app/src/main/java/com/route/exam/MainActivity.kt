a9c0e297200f6cb467429ec6745a86a0bb246bf8
marstagit <96610860+marstagit@users.noreply.github.com>
2022-03-28 00:23:55 +0800

    initial commit

---
+++ app/src/main/java/com/route/exam/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.route.exam
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.route.exam.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
---
+++ app/src/main/java/com/route/exam/detail/model/DetailResponse.kt
@@ -0,0 +1,11 @@
+package com.route.exam.detail.model
+
+import com.google.gson.annotations.SerializedName
+
+class DetailResponse(
+    @SerializedName("data")
+    data:String
+)
+{
+
+}
